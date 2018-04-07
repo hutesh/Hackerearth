108f8a08166d4a2ea3292d01f649d782f3790fcb
hutesh <huteshgauttam@gmail.com>
2018-04-07 14:52:08 +0530

    Good DP (7/4/18)

--- help aswin(DP).cpp
+++ help aswin(DP).cpp
@@ -1,53 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    for(int k=0;k<t;k++)
-    {
-    int n,m;
-    cin>>n>>m;
-    int mat[n][m];
-    int check[n][m];
-    for(int i=0;i<n;i++)
-    {
 
-        for(int j=0;j<m;j++)
-        {
-            cin>>mat[i][j];
-            check[i][j]=0;
-        }
-    }
+int main(){
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int total=0,index=0;
+	int t,n,m;
+	cin>>t;
+	while(t--){
+		cin>>n>>m;
 
-    int maxi=mat[n-1][0];
-     for(int i=1;i<m;i++)
-     {
-         if(mat[n-1][i]>maxi)
-         {
-             maxi=mat[n-1][i];
-             index=i;
-         }
-     }
-     total+=mat[n-1][index];
-     for(int i=n-2;i>=0;i--)
-     {
-         check[i][index]=1;
-         index=-1;
-         maxi=-1;
-         for(int j=0;j<m;j++)
-         {
-             if(check[i][j]!=1&&mat[i][j]>maxi)
-             {
-                 maxi=mat[i][j];
-                 index=j;
-             }
-         }
-         total+=mat[i][index];
-     }
-     cout<<total<<"\n";
-}
-return 0;
+		long long int ar[n][m];
+		for(int i=0;i<n;i++)
+		    for(int j=0;j<m;j++)
+                cin>>ar[i][j];
+
+		for(int i=1; i<n;i++)
+            {
+		    long long int t1=-1,t2=-1;
+		    int index;
+
+		    for(int j=0;j<m;j++)
+                {
+                    if(ar[i-1][j] >= t1)
+                    {
+                        t2 = t1;
+                        t1 = ar[i-1][j];
+                        index = j;
+                    }
+                    else if(ar[i-1][j] > t2)
+                        t2 = ar[i-1][j];
+		        }
+
+                for(int j=0;j<m;j++)
+                {
+                    if(j != index)
+                        ar[i][j] += t1;
+                    else
+                        ar[i][j] += t2;
+                }
+		}
+
+		long long int ans = *max_element(ar[n-1],ar[n-1]+m);
+		cout<<ans<<endl;
+	}
+
+	return 0;
 }
